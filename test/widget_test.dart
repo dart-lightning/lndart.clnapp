8999ecefe2695911296f3441589462138e3c4de7
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-19 17:58:09 +0100

    clnapp: bug fixing
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,9 @@
-import 'package:clnapp/views/home_view.dart';
+import 'package:clnapp/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const HomeView());
+    await tester.pumpWidget(const CLNApp());
   });
 }
