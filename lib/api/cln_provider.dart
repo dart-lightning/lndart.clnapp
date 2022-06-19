f3901d5f7c9f67d263d250f58c4af73a9046344e
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-19 19:09:21 +0100

    clnapp: using getit and introduce some provider to keep alive the grpc
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ lib/api/cln_provider.dart
@@ -0,0 +1,46 @@
+/// CLN provider is a util class that return the instance of CLN wrapper
+/// that works well with the actual platform.
+///
+/// FIXME: This provided need to take some user choise on what the user want
+/// to use.
+import 'dart:io' show Platform;
+import 'package:cln_grpc/cln_grpc.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+enum ClientMode {
+  unixSocket,
+  grpc,
+}
+
+class CLNProvider {
+  static GRPCClient getClient(
+      {required ClientMode mode, Map<String, dynamic> opts = const {}}) {
+    if (kIsWeb) {
+      /// check if we can run on the web, for now NO
+      throw Exception("The actual client did not support the web");
+    }
+
+    if (Platform.isAndroid || Platform.isIOS) {
+      throw Exception("The actual client did not support the mobile App");
+    }
+
+    if (Platform.isWindows && mode == ClientMode.unixSocket) {
+      throw Exception(
+          "The unix socket mode is not supported on Windows platform");
+    }
+    return CLNProvider._buildClient(mode: mode, opts: opts);
+  }
+
+  static GRPCClient _buildClient(
+      {required ClientMode mode, Map<String, dynamic> opts = const {}}) {
+    switch (mode) {
+      case ClientMode.unixSocket:
+        throw Exception("Unix RPC not supported");
+      case ClientMode.grpc:
+        return GRPCClient(
+            rootPath: opts['certificatePath'],
+            host: opts['host'],
+            port: opts['port']);
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
-import 'package:clnapp/views/home_view.dart';
+import 'package:cln_grpc/cln_grpc.dart';
+import 'package:clnapp/api/cln_provider.dart';
+import 'package:clnapp/utils/app_provider.dart';
+import 'package:clnapp/views/app_view.dart';
+import 'package:clnapp/views/home/home_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const CLNApp());
+Future<void> main() async {
+  var provider = await AppProvider().init();
+
+  /// TODO just for now we insert by hand GRPC information, but in the future
+  /// we need to have a Setting UI!
+  var certificateDir = "/media/vincent/VincentSSD/.lightning/testnet";
+  provider.registerLazyDependence<GRPCClient>(
+      () => CLNProvider.getClient(mode: ClientMode.grpc, opts: {
+            'certificatePath': certificateDir,
+            'host': 'localhost',
+            'port': 8001,
+          }));
+  runApp(CLNApp(provider: provider));
 }
 
-class CLNApp extends StatelessWidget {
-  const CLNApp({Key? key}) : super(key: key);
+class CLNApp extends AppView {
+  const CLNApp({Key? key, required AppProvider provider})
+      : super(key: key, provider: provider);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'CLN App',
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      home: HomeView(provider: provider),
     );
   }
 }
