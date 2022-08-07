cd6c6ab20a7fb5603501dca40258e85bab96f2aa
KAVAN-DESAI <kavandesai895@gmail.com>
2022-08-07 23:42:46 +0530

    fix: added a check for settings and initilized provider

--- lib/helper/settings/get_settings.dart
+++ lib/helper/settings/get_settings.dart
@@ -3,15 +3,19 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 Future<Setting> getSettingsInfo() async {
   Setting setting = Setting();
+
   final prefs = await SharedPreferences.getInstance();
+
   if (prefs.getInt('connectionClient') == null) {
     return setting;
   }
+
   setting.connectionType = clients[prefs.getInt('connectionClient')!];
 
   setting.path = prefs.getString('selectedPath')!;
 
   setting.host = prefs.getString('host')!;
+
   setting.nickName = prefs.getString('nickName')!;
 
   return setting;
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:clnapp/api/api.dart';
+import 'package:clnapp/api/client_provider.dart';
+import 'package:clnapp/api/cln/cln_client.dart';
 import 'package:clnapp/constants/user_setting.dart';
 import 'package:clnapp/helper/settings/get_settings.dart';
 import 'package:clnapp/utils/app_provider.dart';
@@ -10,39 +13,37 @@ import 'package:trash_themes/themes.dart';
 Future<void> main() async {
   var provider = await AppProvider().init();
 
-  /// TODO just for now we insert by hand GRPC information, but in the future
-  /// we need to have a Setting UI!
-  // var certificateDir = Platform.environment['CLN_CERT_PATH'];
-  // if (certificateDir == null) {
-  //   throw Exception("Please export the CLN_CERT_PATH for your system");
-  // }
   Setting setting = Setting();
+
   await getSettingsInfo().then((value) => {
         setting = value,
       });
-  // provider.registerLazyDependence<AppApi>(() {
-  //   if(setting.connectionType==clients[0]){
-  //     return CLNApi(
-  //         mode: ClientMode.grpc,
-  //         client: ClientProvider.getClient(mode: ClientMode.grpc, opts: {
-  //           ///FIXME: make a login page and take some path as input
-  //           'certificatePath': setting.path,
-  //           'host': setting.host,
-  //           'port': 8001,
-  //         })
-  //     );
-  //   }
-  //   else{
-  //     return CLNApi(
-  //       mode: ClientMode.unixSocket,
-  //         ///FIXME: make a login page and take some path as input
-  //       client: ClientProvider.getClient(mode: ClientMode.unixSocket, opts: {
-  //         // include the path if you want use the unix socket. N.B it is broken!
-  //         'path': "${setting.path}/lightning-rpc",
-  //       })
-  //     );
-  //   }
-  // });
+
+  if (setting.path != "No path found") {
+    provider.registerLazyDependence<AppApi>(() {
+      if (setting.connectionType == clients[0]) {
+        return CLNApi(
+            mode: ClientMode.grpc,
+            client: ClientProvider.getClient(mode: ClientMode.grpc, opts: {
+              ///FIXME: make a login page and take some path as input
+              'certificatePath': setting.path,
+              'host': setting.host,
+              'port': 8001,
+            }));
+      } else {
+        return CLNApi(
+            mode: ClientMode.unixSocket,
+
+            ///FIXME: make a login page and take some path as input
+            client:
+                ClientProvider.getClient(mode: ClientMode.unixSocket, opts: {
+              // include the path if you want use the unix socket. N.B it is broken!
+              'path': "${setting.path}/lightning-rpc",
+            }));
+      }
+    });
+  }
+
   runApp(CLNApp(provider: provider, setting: setting));
 }
 
