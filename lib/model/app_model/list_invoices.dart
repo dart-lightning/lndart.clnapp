442886dd2cea736ede9e4bd27f15f652ce5738fe
Harshit933 <harshitverma933@gmail.com>
2023-04-04 00:35:29 +0530

    fix: invoices bug in the app
    Previously paid invoices were not shown in the app. This commit fixes the bug.

--- lib/model/app_model/list_invoices.dart
+++ lib/model/app_model/list_invoices.dart
@@ -10,18 +10,19 @@ class AppListInvoices {
   factory AppListInvoices.fromJSON(Map<String, dynamic> json,
       {bool snackCase = false, bool isObject = false, String? status}) {
     var invoices = json.withKey("invoices", snackCase: snackCase) as List;
+    List<AppInvoice> list = [];
+
     if (invoices.isNotEmpty) {
-      var appInvoices = invoices.map((invoice) {
-        var temp = AppInvoice.fromJSON(invoice,
+      for (var invoices in invoices) {
+        AppInvoice temp = AppInvoice.fromJSON(invoices,
             snackCase: snackCase, isObject: isObject);
+
+        /// Filtering the invoices on the basis of status
         if (temp.status == status) {
-          return temp;
+          list.add(temp);
         }
-      }).toList();
-      if (appInvoices.contains(null)) {
-        return AppListInvoices();
       }
-      return AppListInvoices(invoice: appInvoices as List<AppInvoice>);
+      return AppListInvoices(invoice: list);
     } else {
       return AppListInvoices();
     }
@@ -30,69 +31,69 @@ class AppListInvoices {
 
 class AppInvoice {
   /// bolt11 identifier
-  final String bolt11;
+  final String? bolt11;
+
+  final String? bolt12;
 
   /// The quantity of Bitcoin in millisatoshi
   final String amount;
-
-  final String paymentRequest;
-
-  final String prefix;
-
-  final List<int> signature;
-
-  final List<pay_req.TaggedField> tags;
-
-  final String timeStamp;
 
   final String identifier;
 
   final String status;
 
+  final String description;
+
+  final String boltIdentifier;
+
   AppInvoice(
       {required this.bolt11,
+      required this.bolt12,
       required this.amount,
-      required this.paymentRequest,
-      required this.prefix,
-      required this.signature,
-      required this.tags,
-      required this.timeStamp,
       required this.status,
+      required this.description,
+      required this.boltIdentifier,
       this.identifier = "invoice"});
 
   factory AppInvoice.fromJSON(Map<String, dynamic> json,
       {bool snackCase = false, bool isObject = false}) {
     LogManager.getInstance.debug("$json");
-    var bolt11 = json.withKey("bolt11", snackCase: snackCase);
-    var status = json.withKey("status", snackCase: snackCase);
-    pay_req.Bolt11PaymentRequest req = pay_req.Bolt11PaymentRequest(bolt11);
-    LogManager.getInstance.debug(
-        "NEW RESPONSE HERE : ${req.amount} ${req.paymentRequest} ${req.prefix} ${req.signature} ${req.tags} ${req.timestamp}");
-    var amount = req.amount;
-    var paymentRequest = req.paymentRequest;
-    var pre = req.prefix;
-    String prefix = '';
-    if (pre == pay_req.PayRequestPrefix.lntb) {
-      prefix = "lntb";
-    } else if (pre == pay_req.PayRequestPrefix.lnbcrt) {
-      prefix = "lncbrt";
-    } else if (pre == pay_req.PayRequestPrefix.lnbc) {
-      prefix = "lnbc";
+    String amount;
+    String? bolt11;
+    String? bolt12;
+    String boltidentifier;
+
+    /// Checking the type of bolt in the invoices.
+    if (json.withKey("bolt11") == null) {
+      bolt12 = json.withKey("bolt12");
+      boltidentifier = "bolt12";
+
+      /// Assuming the invoice is not paid.
+      amount = json.withKey("amount_msat").toString();
     } else {
-      prefix = "lnsb";
+      bolt11 = json.withKey("bolt11", snackCase: snackCase);
+      pay_req.Bolt11PaymentRequest req = pay_req.Bolt11PaymentRequest(bolt11!);
+
+      /// Amount returned by the package is in bitcoins and amount received from the listinvoices is in msats
+      /// Converting msats into btc
+      amount = (double.parse(req.amount.toString()) * 100000000000).toString();
+      boltidentifier = "bolt11";
     }
-    var signature = req.signature;
-    var tags = req.tags;
-    var timeStamp = req.timestamp;
+    var status = json.withKey("status", snackCase: snackCase);
+
+    /// If the invoice is paid there is no key with "amount_msat" instead there is a key with "amount_recieved_msat"
+    if (status == "paid" && boltidentifier == "bolt12") {
+      amount =
+          json.withKey("amount_received_msat", snackCase: snackCase).toString();
+    }
+    var description = json.withKey("description", snackCase: snackCase);
     return AppInvoice(
       bolt11: bolt11,
+      bolt12: bolt12,
+      boltIdentifier: boltidentifier,
       amount: amount.toString(),
       status: status ?? "unpaid",
-      paymentRequest: paymentRequest,
-      prefix: prefix.toString(),
-      signature: signature,
-      tags: tags,
-      timeStamp: timeStamp.toString(),
+      description: description ?? "No description for this invoice",
     );
   }
 }
