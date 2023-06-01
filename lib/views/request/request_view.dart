6c60f23642acdc19418bd40df4d8ad9770804d6d
Harshit933 <harshitverma933@gmail.com>
2023-06-01 21:00:45 +0530

    fix(errors) : handled potential errors in the request view

--- lib/views/request/request_view.dart
+++ lib/views/request/request_view.dart
@@ -1,10 +1,12 @@
 import 'package:clnapp/api/api.dart';
+import 'package:clnapp/views/home/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:clnapp/components/buttons.dart';
 import 'package:clnapp/model/app_model/generate_invoice.dart';
 import 'package:clnapp/utils/app_provider.dart';
+import 'package:trash_component/components/global_components.dart';
 
 class RequestView extends StatefulWidget {
   final AppProvider provider;
@@ -22,9 +24,12 @@ class _RequestViewState extends State<RequestView> {
     String label = '${DateTime.now()}';
     String description =
         "This is a new description created at${DateTime.now()}";
+
+    /// This could generate an error if the rune have restrictions=readonly
     AppGenerateInvoice response = await widget.provider
         .get<AppApi>()
-        .generateInvoice(label, description, amount: int.parse(display));
+        .generateInvoice(label, description,
+            amount: double.parse(display).floor());
     return response;
   }
 
@@ -106,6 +111,17 @@ class _RequestViewState extends State<RequestView> {
                   icon: Icons.send_outlined,
                   label: "Request",
                   onPress: () async {
+                    ///TODO: Show a warning here when the display value is in decimals
+                    if (double.parse(display).floor() == 0) {
+                      GlobalComponent.showAppDialog(
+                          context: context,
+                          title: 'Please enter a valid amount',
+                          message: 'Enter a amount greater than 0',
+                          closeMsg: 'Close',
+                          imageProvided: const AssetImage(
+                              'assets/images/exclamation.png'));
+                      return;
+                    }
                     AppGenerateInvoice invoiceModel = await generateInvoice();
                     if (context.mounted) {
                       _showBottomDialog(
@@ -124,6 +140,7 @@ class _RequestViewState extends State<RequestView> {
   void _showBottomDialog(
       {required BuildContext context, required AppInvoice invoice}) {
     showModalBottomSheet<void>(
+        backgroundColor: Theme.of(context).disabledColor,
         context: context,
         isScrollControlled: true,
         shape: const RoundedRectangleBorder(
@@ -139,6 +156,26 @@ class _RequestViewState extends State<RequestView> {
                   Expanded(
                     child: Row(
                       children: [
+                        Expanded(
+                            flex: 1,
+                            child: Container(
+                                alignment: Alignment.topLeft,
+                                padding: const EdgeInsets.all(20),
+                                child: IconButton(
+                                    onPressed: () =>
+                                        Navigator.pushAndRemoveUntil(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => HomeView(
+                                                    provider: widget.provider)),
+                                            (route) => false),
+                                    icon: const Icon(
+                                      Icons.home,
+                                      size: 20,
+                                      color: Colors.black,
+                                      weight: 10,
+                                      grade: 10,
+                                    )))),
                         const Spacer(flex: 2),
                         Expanded(
                             flex: 1,
@@ -150,32 +187,60 @@ class _RequestViewState extends State<RequestView> {
                                     icon: const Icon(
                                       Icons.close,
                                       size: 20,
+                                      color: Colors.black,
                                     )))),
                       ],
                     ),
                   ),
                   Expanded(
-                      flex: 2,
+                      flex: 6,
                       child: QrImageView(
                         data: invoice.bolt11,
                         version: QrVersions.auto,
                         backgroundColor: Colors.white,
+                        gapless: false,
                       )),
-                  Expanded(
-                      flex: 1,
-                      child: IconButton(
-                          onPressed: () {
-                            Clipboard.setData(
-                                ClipboardData(text: invoice.bolt11));
-                            const snackBar =
-                                SnackBar(content: Text('Copied to clipboard!'));
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                          },
-                          icon: const Icon(
-                            Icons.copy,
-                            size: 20,
-                          ))),
+                  InkWell(
+                    onTap: () {
+                      Clipboard.setData(ClipboardData(text: invoice.bolt11));
+                      const snackBar = SnackBar(
+                        content: Text('Copied to clipboard!'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width * 0.2,
+                        // margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.41),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(25)),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 25),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.copy),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'Copy invoice',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                   const Spacer(flex: 2),
                 ],
               ));
@@ -189,17 +254,26 @@ class _RequestViewState extends State<RequestView> {
         if (icon != null) {
           /// Handling the exception
           if (display.length == 1) {
-            display = '0';
+            setState(() {
+              display = '0';
+            });
+            return;
           } else {
             display = display.substring(0, display.length - 1);
           }
         }
 
         /// To get only one decimal in the field
-        if (display.contains('.')) {
-          if (value == '.') {
-            return;
-          }
+        if (display.contains('.') && value == '.') {
+          return;
+        }
+
+        /// if value =='.' then the display would be in the form of '.23232'
+        if (display == '0' && value != '.') {
+          setState(() {
+            display = value;
+          });
+          return;
         }
 
         /// Maximum length of 9
@@ -207,7 +281,7 @@ class _RequestViewState extends State<RequestView> {
           return;
         }
         setState(() {
-          display = int.parse(display + value).toString();
+          display = display + value;
         });
       },
       child: Container(
